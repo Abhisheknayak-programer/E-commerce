47754f1f5720f98e4ab3f803d21ee34e64457013
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-03-13 18:15:33 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Nav Bar Function
+const NavFunction = () => {
+    const nav = document.querySelector('.nav-links');
+    const btn = document.querySelector('.burger');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    btn.addEventListener("click",(()=>{
+        nav.classList.toggle("nav-active");
+        // HamBurger Toggler
+        btn.classList.toggle("toogle");
+    }))
+
+
+}
+
+NavFunction();
