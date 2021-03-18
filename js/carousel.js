70fd10787a479f537ff2680c8269b76ce086f43b
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-03-18 08:34:17 +0530

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -10,10 +10,10 @@ $('.owl-carousel').owlCarousel({
             autoplay: true,
         },
         600:{
-            items:3
+            items:1
         },
         1000:{
-            items:4
+            items:1
         }
     }
 })
